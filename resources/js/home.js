51dfb9ade41a9ab21c73a0e3e9bec551d4c8bd37
Ali Muktar <wezyali@gmail.com>
2023-10-25 11:23:09 +0100

    Created four blades and updated home blade

---
+++ resources/js/home.js
@@ -0,0 +1,9 @@
+// Homepage.js
+
+// Add custom styles to the black section
+const blackHeader = document.querySelector('.black-header-back');
+if (blackHeader) {
+    blackHeader.style.backgroundColor = 'black';
+    blackHeader.style.color = 'white';
+    blackHeader.style.padding = '40px';
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head class="hero text-center d-flex align-items-center black-section">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel project</title>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
-  </head>
-  <body>
-    <main>
-        <h1>Here we start</h1>  
-    </main>
-	<script src="index.js"></script>
-  </body>
+    <title></title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#register">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#login">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Home Section -->
+<section id="home" class="hero text-center d-flex align-items-center" style="background-image: url('your-image.jpg');">
+    <div class="container">
+        <h1 class="display-4">Welcome to blogwitter</h1>
+        <p class="lead">Post whatever comes to your mind and regret later</p>
+        <a href="#contact" class="btn btn-primary btn-lg">wonderland</a>
+    </div>
+</section>
+
+<!-- Additional Sections (Add more sections as needed) -->
+<section id="register" class="py-5">
+    <div class="container">
+        <h2>Register</h2>
+        <p>Register for access to our services.</p>
+    </div>
+</section>
+
+<section id="login" class="bg-light py-5">
+    <div class="container">
+        <h2>Login</h2>
+        <p>Login to your account.</p>
+    </div>
+</section>
+
+<section id="about" class="py-5">
+    <div class="container">
+        <h2>About Us</h2>
+        <p>Your about section content goes here.</p>
+    </div>
+</section>
+
+<!-- Include Bootstrap and JavaScript (jQuery, Popper.js, Bootstrap.js) scripts -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 </html>
