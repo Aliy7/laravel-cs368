c691354cc3ee85427c8e2906094739b36ba98ea4
Ali Muktar <wezyali@gmail.com>
2023-10-26 11:22:11 +0100

    created post and comment models

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model{
+    use HasFactory;
+
+    protected $user_post =[
+        'post_id',
+        'post_content',
+        'image_url',
+        'post_date',
+        'post_time'
+
+
+    ];
+}
--- database/migrations/create_user_table.php
+++ database/migrations/create_user_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id(); //add value by 1 automatically 
-            $table->string('userName');
+            $table->string('user_name');
             $table->string('email')->unique();
-            $table -> string('firstName');
-            $table -> string('LastName');
-            $table -> string('dateOfBirth')-> nullable();
+            $table -> string('first_name');
+            $table -> string('Last_name');
+            $table -> string('date_of_Birth')-> nullable();
             $table -> string('phone_number') -> nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
