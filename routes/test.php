4e943141d0a14ae5771daf4329a574dc1ed806b8
Ali Muktar <wezyali@gmail.com>
2023-11-20 00:10:09 +0000

    Added mailtrap password resetting

---
+++ routes/test.php
@@ -0,0 +1,10 @@
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/send-test-mail', function () {
+    Mail::raw('This is a test email', function ($message) {
+        $message->to('test@example.com')->subject('Test Email');
+    });
+
+    return 'Email sent successfully!';
+});
